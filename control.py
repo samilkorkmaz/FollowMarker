3e93e78160c0aab0537d83dee97e06a9f39097dd
samil korkmaz <samil.korkmaz@gmail.com>
2021-03-03 14:14:25 +0200

    typo

--- control.py
+++ control.py
@@ -8,10 +8,10 @@ def generateCommands(refLoc, currentLoc, timeStep_s):
     dx = refLoc[0] - currentLoc[0]
     dy = refLoc[1] - currentLoc[1]
     errorStr = "dx = %1.0f, dy = %1.0f" % (dx, dy)
-    propX = Kp*dx #proportianl term in x direction
+    propX = Kp*dx #proportional term in x direction
     integX = integX + Ki*dx*timeStep_s #integral term in x direction
     forceX = propX + integX #force in x direction
-    propY = Kp*dy #proportianl term in y direction
+    propY = Kp*dy #proportional term in y direction
     integY = integY + Ki*dy*timeStep_s #integral term in y direction
     forceY = propY + integY #force in y direction
     return [forceX, forceY, errorStr]

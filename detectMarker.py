8f94c9766bfcf01202f949eec574d786fde82b14
Şamil Korkmaz <samil.korkmaz@gmail.com>
2021-03-07 19:54:32 +0300

    Update detectMarker.py

--- detectMarker.py
+++ detectMarker.py
@@ -1,7 +1,6 @@
+#https://www.youtube.com/watch?v=v5a7pKSOJd8
 import cv2
 import cv2.aruco as aruco
-import numpy as np
-import os
 
 def findMarker(img, markerSize=6, totalMarkers=250, draw=True):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -9,7 +8,16 @@ def findMarker(img, markerSize=6, totalMarkers=250, draw=True):
     arucoDict = aruco.Dictionary_get(key)
     arucoParam = aruco.DetectorParameters_create()
     bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
-    print(ids)
+    if len(bboxs) > 0:
+        xSum = 0
+        ySum = 0
+        for i in range(4):
+            xSum += bboxs[0][0][i][0]
+            ySum += bboxs[0][0][i][1]
+        xCenter = xSum/4
+        yCenter = ySum/4
+        print(bboxs[0][0])
+        print("xCenter:",xCenter,", yCenter:", yCenter)
     if draw:
         aruco.drawDetectedMarkers(img, bboxs)
 

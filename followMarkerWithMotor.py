3bdd4cdda0e9606fe8365b203da9339f412e3a09
Şamil Korkmaz <samil.korkmaz@gmail.com>
2021-03-17 18:55:07 +0300

    Add stop motors

--- followMarkerWithMotor.py
+++ followMarkerWithMotor.py
@@ -63,6 +63,8 @@ try:
             image = captureVideo.frame.copy()
             image = cv2.putText(image, errorStr, (0,100), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2, lineType=cv2.LINE_AA) 
             cv2.circle(image, (xArucoMarkerCenter, yArucoMarkerCenter), radius, circleColor, circleThickness)
+        else:
+            setForce(0, 0, 0, 0) #stop motors
         cv2.drawMarker(image, (int(cameraCenterX), int(cameraCenterY)), (255,0,0), markerType=cv2.MARKER_CROSS, markerSize=20, thickness=4, line_type=cv2.LINE_AA)
         cv2.imshow("Robust", image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
